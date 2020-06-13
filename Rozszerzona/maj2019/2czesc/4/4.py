c8386178e54487e98980ad4f4f38b3972b5d5d61
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2020-06-13 15:37:00 +0200

    43

--- Rozszerzona/maj2019/2czesc/4/4.py
+++ Rozszerzona/maj2019/2czesc/4/4.py
@@ -5,7 +5,6 @@ import math
 
 
 def ex1():
-
     def ispower(el):
         number = 1
         while number<el:
@@ -13,15 +12,12 @@ def ex1():
         if number == el:
             return True
         return False
-
     data = open("Rozszerzona/maj2019/2czesc/Dane_PR2/liczby.txt","r", encoding="utf-8").read().splitlines()
     result = list()
-
     for i in data:
         if ispower(int(i)):
             result.append(i)
     return result
-
 
 def ex2():
     def silnia(n):
@@ -31,11 +27,8 @@ def ex2():
         for i in range(1,n+1,1):
             res*=i
         return res
-
-
     data = open("Rozszerzona/maj2019/2czesc/Dane_PR2/liczby.txt","r", encoding="utf-8").read().splitlines()
     result = list()
-
     for i in data:
         summary = int(0)
         for x in list(i):
@@ -44,11 +37,35 @@ def ex2():
             result.append(i)
     return result
 
-    
+def ex3():
+    def nwd(a,b):
+        while b!=0:
+            tmp = a%b
+            a = b
+            b = tmp
+        return a
+
+    data = open("Rozszerzona/maj2019/2czesc/Dane_PR2/liczby.txt","r", encoding="utf-8").read().splitlines()
+    result = list()
+    i = 0
+    while i<len(data):
+        elem = int(data[i])
+        counter = list()
+        while nwd(elem,int(data[i]))>1 and i<len(data)-1:
+            counter.append(elem)
+            elem = nwd(elem,int(data[i]))
+            i+=1
+        if len(counter)>len(result):
+            result = counter
+        i+=1
+    return result
+
+
 
 def main():
     # print(len(ex1()))
-     print(ex2())
+    # print(ex2())
+    print(len(ex3()))
 
 if __name__ == "__main__":
     main()
