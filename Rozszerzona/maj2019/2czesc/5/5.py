e7f363c1252ba9a3a558c20670cf241e8f239ba0
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-06-14 13:27:34 +0200

    5.2 problem

--- Rozszerzona/maj2019/2czesc/5/5.py
+++ Rozszerzona/maj2019/2czesc/5/5.py
@@ -4,28 +4,60 @@ def splitToTab(line):
 
 class Day:
     def __init__(self,dzien,temperatura,opad,kat_chmur,wiel_chmur):
-        self.dzien = dzien
-        self.temperatura = temperatura
-        self.opad = opad 
-        self.kat_chum = kat_chmur
-        self.wiel_chmur = wiel_chmur
+        self.dzien = int(dzien)
+        self.temperatura = float(temperatura.replace(",","."))
+        self.opad = int(opad) 
+        self.kat_chmur = str(kat_chmur)
+        self.wiel_chmur = int(wiel_chmur)
 
     def __str__(self):
         return f'{self.dzien, self.temperatura, self.opad, self.kat_chmur, self.wiel_chmur}'
 
 def ex1(days):
-    pass
+    counter = int(0)
+    for i in days:
+        if float(i.temperatura) >= float(20) and int(i.opad) <= int(5):
+            counter+=1
+    return counter
+
+def ex2(days):
+    result = list()
+    i = 0
+    while i < len(days)-1:
+        counter = list()
+        temp = days[i].temperatura
+        counter.append(days[i])
+        i+=1
+        while days[i].temperatura > temp:
+            temp = days[i].temperatura
+            counter.append(days[i])
+            i+=1
+            if i==len(days):
+                break
+        if len(counter)>len(result):
+            result = counter
+        i+=1
+
+    for i in result:
+        print(i)
+    return {result[0].dzien, result[len(result)-1].dzien}
 
 
 
 def main():
     data = open('Rozszerzona/maj2019/2czesc/Dane_PR2/pogoda.txt','r',encoding='utf-8').read().splitlines()
     days = list()
-    for i in data:
-        tab = splitToTab(i)
+    for i in range(1,len(data),1):
+        tab = splitToTab(data[i])
         days.append(Day(tab[0],tab[1],tab[2],tab[3],tab[4]))
-    print(days)
+        """print(days[i-1])
+        ex = input()"""
     
+    
+    first_ex = ex1(days)
+    print(first_ex)
+    second_ex = ex2(days)
+    print(second_ex)
 
 if __name__ == "__main__":
     main()
