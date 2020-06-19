b023c3361dcaf8bd00ef0b4fac6333a8d4c17498
Jakub Lemiesiewicz <49617551+JakubLem@users.noreply.github.com>
2020-06-20 00:13:50 +0200

    incorrect

--- Rozszerzona/maj2016/2czesc/6/6.py
+++ Rozszerzona/maj2016/2czesc/6/6.py
@@ -1,6 +1,36 @@
 
+def szyfruj(word,key):
+    asciiArray = list()
+    for i in word:
+        asciiArray.append(ord(i))
+    print("ascciArray:", asciiArray)
+    siphrArray = list()
+    for i in asciiArray:
+        if int(int(i)+int(key)) > 90:
+            siphrArray.append(int(i)-(90-int(i)+1))
+        else:
+            siphrArray.append(int(i)+int(key))
+    """INCORRECT"""
+    """INCORRECT"""
+
+    """INCORRECT"""
+    """INCORRECT"""
+    """INCORRECT"""
+    """INCORRECT"""
+    """INCORRECT"""
+    
+    result = str("")
+    for i in siphrArray:
+        result += str(chr(i))
+    return result
+
 def ex_1():
-    pass
+    word_array = ["INTERPRETOWANIE"]
+    key = 107
+    result = list()
+    for i in word_array:
+        result.append(szyfruj(i,key))
+    return result
 
 def ex_2():
     pass
@@ -10,7 +40,8 @@ def ex_3():
 
 
 def main():
-    pass
+    ex1 = ex_1()
+    print(ex1)
 
 if __name__ == "__main__":
     main()
