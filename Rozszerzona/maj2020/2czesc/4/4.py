fd5db361c830c548dda62a1313e18ee32b74f677
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-06-28 19:59:39 +0200

    rows read 4

--- Rozszerzona/maj2020/2czesc/4/4.py
+++ Rozszerzona/maj2020/2czesc/4/4.py
@@ -1,5 +1,14 @@
+class Row:
+    def __init__(self,number,word):
+        self.number = number
+        self.word = word
 
-def ex1():
+    def __str__(self):
+        return f'{self.number, self.word}'
+
+
+
+def ex1(rows):
     pass
 
 def ex2():
@@ -9,8 +18,18 @@ def ex3():
     pass
 
 
+def getRows(path):
+    rows = list()
+    data = open(path, "r", encoding="utf-8").read().splitlines()
+    for i in data:
+        obj = i.split()
+        rows.append(Row(int(obj[0]),obj[1]))
+    return rows
+
 def main():
-    pass
+    rows = getRows("Rozszerzona/maj2020/2czesc/Dane_PR2/pary.txt")
+    
+    ex_1 = ex1(rows)
 
 if __name__ == "__main__":
     main()
