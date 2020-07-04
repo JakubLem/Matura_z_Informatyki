2f3095b37e6fcea1e1a2e3f9e6cc5718284defcf
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-07-04 21:39:41 +0200

    4.2

--- Rozszerzona/maj2018/2czesc/4/4.py
+++ Rozszerzona/maj2018/2czesc/4/4.py
@@ -6,6 +6,22 @@ def ex1(rows):
         result += rows[i][9]
     return result
 
+def ex2(rows):
+    word = str()
+    maincounter = int(0)
+    for i in rows:
+        counter = int(0)
+        charlist = list()
+        for char in i:
+            if char is not in charlist:
+                counter += 1
+                charlist.append(char)
+        if counter > maincounter:
+            maincounter = counter
+            word = i
+    return {'word' : word, 'counter' : maincounter}
+
+
 
 def main():
     rows = open("Rozszerzona/maj2018/2czesc/Dane_PR2/sygnaly.txt", "r", encoding="utf-8").read().splitlines()
