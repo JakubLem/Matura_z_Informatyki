f7bd03fc59b7195a89019bf7a5c5d7eb2f78b190
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-07-04 23:32:13 +0200

    5.1

--- Rozszerzona/maj2018/2czesc/5/5.py
+++ Rozszerzona/maj2018/2czesc/5/5.py
@@ -1,7 +1,25 @@
+class Day:
+    def __init__(self,date,water):
+        self.date = date
+        self.water = water
 
+    def __str__(self):
+        return f'{self.date, self.water}'
+
+
+def ex1():
+    pass
 
 def main():
-    pass
+    data = open("Rozszerzona/maj2018/2czesc/Dane_PR2/woda.txt", "r", encoding="utf-8").read().splitlines()
+    days = list()
+    for i in data:
+        day = i.split()
+        date = day[0]
+        water = day[1]
+
+        print(i.split())
+    
 
 if __name__ == "__main__":
     main()
