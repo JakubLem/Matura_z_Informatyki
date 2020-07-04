093df789c4302a854be9660e020b34a0639343f1
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-07-04 21:28:10 +0200

    4.1

--- Rozszerzona/maj2018/2czesc/4/4.py
+++ Rozszerzona/maj2018/2czesc/4/4.py
@@ -1,8 +1,17 @@
 
 
+def ex1(rows):
+    result = str("")
+    for i in range(39,len(rows),40):
+        result += rows[i][9]
+    return result
+
 
 def main():
-    pass
+    rows = open("Rozszerzona/maj2018/2czesc/Dane_PR2/sygnaly.txt", "r", encoding="utf-8").read().splitlines()
+    ex_1 = ex1(rows)
+    print(ex_1)
+
 
 if __name__ == "__main__":
     main()
