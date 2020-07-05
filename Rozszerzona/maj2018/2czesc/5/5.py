701ab534c40f9df9f1fb3b6d2afe562ecf3526c7
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-07-05 10:45:58 +0200

    ex1

--- Rozszerzona/maj2018/2czesc/5/5.py
+++ Rozszerzona/maj2018/2czesc/5/5.py
@@ -6,9 +6,27 @@ class Day:
     def __str__(self):
         return f'{self.date, self.water}'
 
+    def toYear(self):
+        return int(self.date[0:4])
 
-def ex1():
-    pass
+
+def ex1(days):
+    year = 2008
+    resultyear = 2008
+    result = 0
+    i = 0
+    while i < len(days):
+        counter = int(0)
+        while days[i].toYear() == year and i < len(days):
+            i += 1
+            counter += int(days[i].water)
+            pass
+        if counter > result:
+
+
+
+    
+
 
 def main():
     data = open("Rozszerzona/maj2018/2czesc/Dane_PR2/woda.txt", "r", encoding="utf-8").read().splitlines()
@@ -17,9 +35,13 @@ def main():
         day = i.split()
         date = day[0]
         water = day[1]
-
-        print(i.split())
+        days.append(Day(date,water))
     
+    for i in days:
+        print(i.toYear())
+
+    ex_1 = ex1(days)
+    print(ex_1)
 
 if __name__ == "__main__":
     main()
